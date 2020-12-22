a2e83c35d5016020b69377ea528cd910bad9c67e
Pedro Ignacio <pedroig100.pi@gmail.com>
2020-12-22 20:56:41 -0300

    Ajustando o código para deploy

--- src/components/form.js
+++ src/components/form.js
@@ -63,10 +63,6 @@ const Inputs = ({ regions }) => {
         filtrarPais(country)
     }, [country, countriesBackup, setCountries])
 
-    const colorStyles = {
-
-    }
-
     return(
         <>
             <input 
